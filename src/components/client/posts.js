804d48efd0b4cc01664716cb4545bed4b03219db
Shayan Jafari <shayanja12345@gmail.com>
2022-10-22 18:53:09 +0330

    complete card style

--- src/components/client/posts.js
+++ src/components/client/posts.js
@@ -14,7 +14,6 @@ const Posts = () => {
     .get(`${clientConfig.siteUrl}/wp-json/wp/v2/posts`)
     .then((res) => {
       setData(res.data);
-      // console.log(res.data, "is response");
     })
     .catch((err) => console.error(err, "is error!"));
 
@@ -23,13 +22,32 @@ const Posts = () => {
       <Row>
         {data.map((post) => {
           return (
-            <Col sm={12} xs={12} md={6} xl={3} lg={4}>
+            <Col className="mb-2" sm={12} xs={12} md={6} xl={3} lg={4}>
               <Card>
-                <CardImg />
+                <CardImg
+                  alt={post.title.rendered}
+                  className="posts-img"
+                  src={
+                    post.uagb_featured_image_src.medium[0]
+                      ? post.uagb_featured_image_src.medium[0]
+                      : "assets/img_not_found.png"
+                  }
+                  width="100%"
+                  height="200px"
+                />
                 <CardBody>
                   {post.title.rendered}
                   <Divider />
-                  {renderHTML(post.content.rendered)}
+                  <Paragraph
+                    ellipsis={{
+                      rows: 2,
+                      expandable: false,
+                      symbol: "ادامه مطلب",
+                    }}
+                  >
+                    <p>{renderHTML(post.content.rendered)}</p>
+                  </Paragraph>
+                  <span>ادامه مطلب...</span>
                 </CardBody>
               </Card>
             </Col>
